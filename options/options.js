49744c7bf1d12a2ef408205c191fbc6b7eb33606
cruz636 <juanbarcruz96@gmail.com>
2023-04-02 02:14:26 -0300

    Initial

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testpino-49",
+  url: "https://testpino-49.botics.co",
+  api: "https://testpino-49.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
